799df801153304824e1fcc273d0dc13daff51569
Kalyan Shilpa Rani <kalyanshilpa2003@gmail.com>
2025-11-29 15:16:10 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
 
+// Navigation Menu
+const navLinks = document.querySelectorAll('.nav-menu .nav-link');
+const menuOpenButton = document.querySelector('#menu-open-button');
+const menuCloseButton = document.querySelector('#menu-close-button');
+
+menuOpenButton.addEventListener('click', () => {
+  document.body.classList.add('menu-open');
+});
+
+menuCloseButton.addEventListener('click', () => {
+  document.body.classList.remove('menu-open');
+});
+
+// Close menu when a nav link is clicked
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('menu-open');
+  });
+});
+
+// Swiper Slider
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
